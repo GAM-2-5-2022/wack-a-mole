05142aca7c55729a25955db8b740b8723ca6aaee
Eugen-Kulic-Golja <105432637+Eugen-Kulic-Golja@users.noreply.github.com>
2022-06-03 12:19:32 +0200

    Add files via upload

--- wack-a-mole/wack-a-mole.py
+++ wack-a-mole/wack-a-mole.py
@@ -1,11 +1,30 @@
 from tkinter import *
+from tkinter.ttk import *
+import random
+import time
+
+lives = 3
+score = 0
 
 top = Tk()
 
 top.geometry("900x1000")
 
-c = Canvas(top, bg = "green", height = "900", width = 1000)
+c = Canvas(top, bg = "green", height = "900", width = "1000")
 
-c.pack
+c.pack()
+
+b = Button(top,text = "Simple") 
+photo = PhotoImage(file = r"C:\Users\Desktop\icon.png") 
+b.pack()
+def button_click():
+    score += 1
+
+   
+#while lives > 0:
+    #btn = tt.Button(top, image="mole.png", command=button_click)
+    
+    
 
 top.mainloop()
+
