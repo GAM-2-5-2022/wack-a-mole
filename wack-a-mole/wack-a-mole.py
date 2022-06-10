11ef3f3e61374139bed6f589499faf3bc015fd60
Eugen-Kulic-Golja <105432637+Eugen-Kulic-Golja@users.noreply.github.com>
2022-06-10 18:20:30 +0200

    Update wack-a-mole.py

--- wack-a-mole/wack-a-mole.py
+++ wack-a-mole/wack-a-mole.py
@@ -12,18 +12,21 @@ top.geometry("900x1000")
 c = Canvas(top, bg = "green", height = "9000", width = "10000")
 c.place(x=0, y=0)
 
-
+photo = PhotoImage(file = r"mole.png")
 def click():
-    time.sleep(1)
-    b.pack_forget()
-while x<2:
-    time.sleep(1)
-    b = Button(top, command=click)
+    global photo
+    global b
+    b.destroy()
+    time.sleep(0.2)
+    b = Button(top,image=photo, command=click)
     b.place(x=random.randint(100, 800), y=random.randint(200, 800))
-    x +=1
-print(x)
+    
+def btn():
+    global b
+    b = Button(top,image=photo, command=click)
+    b.place(x=random.randint(100, 800), y=random.randint(200, 800))
 
+btn()
 
 top.mainloop()
-
 
